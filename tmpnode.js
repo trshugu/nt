d95f49b9c0164747d8e28faea32d95aa60ddd5fd
nodeman <att@atatjia.comcom>
2013-01-24 12:06:32 +0900

    'nodereinstall'

---
+++ tmpnode.js
@@ -0,0 +1,30 @@
+/*
+*/
+
+
+
+
+
+
+
+
+
+/*
+// もっともシンプルなhttpサーバー
+var http = require('http');
+
+http.createServer(function (req, res)
+{
+  res.writeHead( 200, {'Content-Type': 'text/plain'} );
+  res.end( '地獄世界\n' );
+})
+.listen(1337, "127.0.0.1");
+
+console.log('Server running at http://127.0.0.1:1337/');
+*/
+
+
+/*
+// nodejsのまま使う方面
+console.log("hell world");
+*/
