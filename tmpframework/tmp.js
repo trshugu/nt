ca094f3ffb69fe474d69d55bbd0a8df702b54632
rtrs <a@a.a>
2013-06-28 16:52:58 +0900

    ''20130628

--- tmpframework/tmp.js
+++ tmpframework/tmp.js
@@ -1,10 +1,87 @@
 /*
 */
 $(document).ready(function() {
-	$("#tmp");
+  $("#tmp");
 });
 
 
+
+
+
+/*
+// angular ìÆìIÇ…ÉÇÉfÉãèëÇ´ä∑Ç¶
+function helloCtrl($scope)
+{
+  $scope.hello = "Hello, World";
+  $scope.goodbye = function()
+  {
+    $scope.hello = "Good Bye";
+  }
+}
+*/
+
+/*
+// angular
+$(document).ready(function() {
+  $(function(){
+    var elem_in=$("input[name='yourname']"),
+    elem_out=$("#yourname");
+    
+    var showName = function(name){
+      elem_out.text(name);
+    };
+    
+    showName(elem_in.val());
+    
+    elem_in.bind('keyup', function(){
+      setTimeout(showName,  0, $(this).val());
+    });
+  });
+});
+*/
+
+
+/*
+// Backbone Collection
+var obj = new Backbone.Model();
+obj.set({name: "Murata"});
+obj.set({age: 20});
+var obj2 = new Backbone.Model({name: "Kenichiro1", age: 30});
+var obj3 = new Backbone.Model({name: "Kenichiro2", age: 11});
+var obj4 = new Backbone.Model({name: "Kenichiro3", age: 3});
+
+var objs = new Backbone.Collection([obj, obj2, obj3, obj4]);
+//console.log("objs: " + JSON.stringify(objs));
+
+//console.log("objs.get(cid): " + JSON.stringify(objs.get("c1")));
+//console.log("objs.at(index): " + JSON.stringify(objs.at(0)));
+
+//objs.comparator = function(item) {
+//    return item.get("age");
+//};
+ 
+//objs.sort();
+//console.log("After sort objs: " + JSON.stringify(objs));
+//console.log("After sort objs.at(index): " + JSON.stringify(objs.at(0)));
+
+// map
+tmpObj = objs.map(function(item){
+  return item.get("age") + 1;
+});
+console.log("map result: " + JSON.stringify(tmpObj));
+
+// reduce
+tmpObjred = objs.reduce(function(memo, item){
+  return memo + item.get("age") + 1 ;
+}, 0);
+console.log("reduce result: " + JSON.stringify(tmpObjred));
+
+// pluck
+console.log("pluck result: " + JSON.stringify(objs.pluck("name"))); 
+*/
+
+
+/*
 // Backbone Modelâûóp
 var Staff = Backbone.Model.extend({
   defaults: {
@@ -23,6 +100,7 @@ tmpStaff.set({name: "Murata", age: 15, id: 101});
 console.log("Staff[" + tmpStaff.cid + "]: " + JSON.stringify(tmpStaff));
 
 var tmpStaff2 = new Staff({name: "Kenichiro", age: 35, id: 102});
+*/
 
 
 /*
